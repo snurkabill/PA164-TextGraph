bf670efb77f54972407def8b0b859c9d15d051cb
Jiří Vahala <snurkabill@gmail.com>
2017-01-19 12:28:51 +0100

    consistent r script

--- run.R
+++ run.R
@@ -1,5 +1,9 @@
 path = "d:/devel/FI_JAVA/PA164/"
 setwd(path)
-out=system(paste("java -jar target/text-graph-1.0-SNAPSHOT.jar", path, "100000 10 7"), intern=TRUE, wait = TRUE)
+out = system(
+	paste("java -jar target/text-graph-1.0-SNAPSHOT.jar", path, "100000 10 7"),
+	intern = TRUE,
+	wait = TRUE
+	)
 
 
